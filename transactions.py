a1e87dd1a2a82d220b413924230966d54d80e41c
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2023-07-14 00:43:16 +0300

    Update transactions.py
    Additional function for user-friendly context for transactions

--- transactions.py
+++ transactions.py
@@ -1,6 +1,7 @@
 import requests
 import base64
 
+# Function returns list of decoded transactions if GET status is "200: OK", otherwise returns None
 def get_block_transactions(block_number):
     base_url = "https://akash-mainnet-rpc.cosmonautstakes.com/block?height="
     url = base_url + str(block_number)
@@ -16,11 +17,17 @@ def get_block_transactions(block_number):
     else:
         return None
 
-# Example of function's usage on block 11260637
-block_number = 11260637
-transactions = get_block_transactions(block_number)
-if transactions:
-    for tx in transactions:
-        print(tx)
-else:
-    print("No transactions in block", block_number)
+# Function adds user-friendly context for transactions
+def view_block_transactions(txs_list, block_number):
+    if txs_list:
+        print("There are", len(txs_list), "transactions in block", block_number)
+        for tx in txs_list:
+            print(tx)
+    else:
+        print("Couldn't get transactions from block", block_number)
+
+# Example of both functions on block 11260637
+if __name__ == "__main__":
+    block_number = 11260637
+    transactions = get_block_transactions(block_number)
+    view = view_block_transactions(transactions, block_number)
